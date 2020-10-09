a915f0190f3e2424da17d7d6396865b64bd86454
Martin-Sparks <martin.sparks82@gmail.com>
2020-10-09 12:22:31 +0100

    final commit

--- script.js
+++ script.js
@@ -27,12 +27,23 @@ function updatePlayIcon(){
 
 function updateProgress(){
    progress.value = (video.currentTime / video.duration) * 100;
+
+   let mins = Math.floor(video.currentTime / 60);
+   if(mins < 10) {
+        mins = '0' + String(mins);
+   }
+   let secs = Math.floor(video.currentTime % 60);
+   if(secs < 10) {
+        secs = '0' + String(secs);
+   }
+
+   timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // set video time to progress
 
 function setVideoProgress(){
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // stop video
